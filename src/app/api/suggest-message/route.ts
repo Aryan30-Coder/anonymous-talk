5a11b4b56802de02be08cd834ea20ed06a46608b
Aryan <301205ap@gmail.com>
2026-02-01 21:56:15 +0530

    Integration of AI for suggestions

---
+++ src/app/api/suggest-message/route.ts
@@ -0,0 +1,31 @@
+import { streamText } from "ai";
+import { groq } from "@ai-sdk/groq";
+
+export const runtime = "edge";
+
+export async function POST() {
+  try {
+    const prompt = `
+Create a list of three open-ended and engaging questions formatted as a single string.
+Each question should be separated by '||'.
+
+These questions are for an anonymous social messaging platform like Qooh.me.
+Avoid personal or sensitive topics.
+Focus on universal themes that encourage friendly interaction.
+
+Example output:
+"What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?"
+`;
+
+    const result = streamText({
+      model: groq("llama-3.1-70b-versatile"),
+      prompt,
+      maxOutputTokens: 400
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error("An unexpected error occurred!", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
