23fe96d23b5184766330e5b39a179d2bdb50a6c0
Aryan <301205ap@gmail.com>
2026-02-02 23:02:27 +0530

    Made components for dashboard page

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const pages = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+    </div>
+  )
+}
+
+export default pages
---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,106 @@
+'use client'
+import{ useState } from 'react'
+import {zodResolver} from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import Link from 'next/link'
+import { toast } from 'sonner'
+import { useRouter } from 'next/navigation'
+import {Form, FormField, FormItem, FormLabel, FormDescription, FormMessage} from '@/components/ui/form'
+import { Button } from '@/components/ui/button'
+import { Loader2 } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+import { signInSchema } from '@/Schemas/signInSchema'
+import { signIn } from 'next-auth/react'
+
+
+const page = () => {
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: '',
+      password: ''
+    }
+  })
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const result = await signIn('credentials', {
+        redirect: false,
+        identifier: data.identifier,
+        password: data.password
+    })
+
+    if(result?.error){
+        toast.error("Incorrect username or password!")
+    }
+
+    if(result?.url){
+        router.replace('/dashboard')
+    }
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Anonymous-Talk
+          </h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <Input {...field} name="email" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} name="password" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className='w-full' disabled={isSubmitting}>
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page
